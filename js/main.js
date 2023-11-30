be43fddc69c119fc27ac78c459c1c0490de0c5f0
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-30 18:21:08 +0100

    added score user on js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,57 @@
-const arrayNum = [];
-const arrayRand = document.getElementById("arrayRandom")
+let arrayNum = [];
+const arrayRand = document.getElementById("arrayRandom");
+const punteggio = document.getElementById("score");
 
-for (let i = 0; i < 5; i++) {
-    arrayNum.push(genRandomNum(1, 10));
-}
-arrayRand.innerHTML = arrayNum;
+let seconds = 3;
+let tentativi = 5;
+let score = 0;
+
+arrayNum = genArrayNumRand(arrayNum);
 console.log(arrayNum);
+arrayRand.append(arrayNum);
 
 
-setTimeout(function () {
-    addHidden(arrayRand, "hidden");
-}, 30000);
+setTimeout(
+    function () {
+        addHidden(arrayRand, "hidden");
+    },
+    seconds * 1000
+);
+
+const btn_play = document.getElementById("btn_play");
+btn_play.addEventListener("click",
+    function () {
+        let esatte = [];
+        let i = 0;
+        while (i <= 5) {
+            const userNum = parseInt(prompt("Inserisci Numero"));
+            console.log(userNum);
+            if (arrayNum.includes(userNum)) {
+                score++;
+                esatte.push(userNum);
+            }
+            i++;
+        }
+        punteggio.innerHTML = "Il tuo punteggio è " + score + " e hai indovinato " + esatte + ".";
+    }
+)
 
 
 
-function addHidden(tagName, className){
-    const element = tagName.classList.add(className);
+
+
+function genArrayNumRand(array) {
+    while (array.length < 5) {
+        let temp = genRandomNum(1, 10);
+        if (!array.includes(temp)) {
+            array.push(temp);
+        }
+    }
+    return array;
+}
+
+function addHidden(tagName, className) {
+    tagName.classList.add(className);
 }
 
 function genRandomNum(min, max) {
