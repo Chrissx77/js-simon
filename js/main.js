01b26cd4770bf9558e63df2228c459bec10294ca
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-30 15:22:41 +0100

    added timer 30 seconds&class hidden to array on js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 const arrayNum = [];
 const arrayRand = document.getElementById("arrayRandom")
 
-for(let i = 0; i < 5; i++){
+for (let i = 0; i < 5; i++) {
     arrayNum.push(genRandomNum(1, 10));
 }
 arrayRand.innerHTML = arrayNum;
 console.log(arrayNum);
 
 
+setTimeout(function () {
+    addHidden(arrayRand, "hidden");
+}, 30000);
 
-function genRandomNum(min, max){
+
+
+function addHidden(tagName, className){
+    const element = tagName.classList.add(className);
+}
+
+function genRandomNum(min, max) {
     let result;
-    result = Math.floor(Math.random() * (max - min +1)) + min;
+    result = Math.floor(Math.random() * (max - min + 1)) + min;
     return result;
 }
